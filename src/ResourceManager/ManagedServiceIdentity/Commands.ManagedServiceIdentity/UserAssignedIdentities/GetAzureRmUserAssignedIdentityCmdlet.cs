b79395d7bd02d72c4b3e906a0362636dd96dd08a
Varun Kumar <vakuma@microsoft.com>
2018-01-05 15:42:20 -0800

    Updating Get to enumerate collection from the next link

--- src/ResourceManager/ManagedServiceIdentity/Commands.ManagedServiceIdentity/UserAssignedIdentities/GetAzureRmUserAssignedIdentityCmdlet.cs
+++ src/ResourceManager/ManagedServiceIdentity/Commands.ManagedServiceIdentity/UserAssignedIdentities/GetAzureRmUserAssignedIdentityCmdlet.cs
@@ -1,4 +1,5 @@
-﻿using System.Management.Automation;
+﻿using System.Linq;
+using System.Management.Automation;
 using Microsoft.Azure.Commands.ManagedServiceIdentity.Common;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Management.ManagedServiceIdentity.Models;
@@ -6,7 +7,7 @@ using Microsoft.Azure.Management.ManagedServiceIdentity.Models;
 namespace Microsoft.Azure.Commands.ManagedServiceIdentity.UserAssignedIdentities
 {
     [Cmdlet(VerbsCommon.Get, "AzureRmUserAssignedIdentity")]
-    [OutputType(typeof(Identity))]
+    [OutputType(typeof (Identity))]
     public class GetAzureRmUserAssignedIdentityCmdlet : MsiBaseCmdlet
     {
         [Parameter(
@@ -35,23 +36,42 @@ namespace Microsoft.Azure.Commands.ManagedServiceIdentity.UserAssignedIdentities
                     var result =
                         this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities
                             .ListBySubscriptionWithHttpMessagesAsync().GetAwaiter().GetResult();
+                    var resultList = result.Body.ToList();
+                    var nextPageLink = result.Body.NextPageLink;
+                    while (!string.IsNullOrEmpty(nextPageLink))
+                    {
+                        var pageResult =
+                            this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities
+                                .ListBySubscriptionNextWithHttpMessagesAsync(nextPageLink).GetAwaiter().GetResult();
+                        resultList.AddRange(pageResult.Body.ToList());
+                        nextPageLink = pageResult.Body.NextPageLink;
+                    }
 
-                    WriteObject(result.Body);
+                    WriteObject(resultList, true);
                 }
                 else if (string.IsNullOrEmpty(this.Name))
                 {
                     var result =
                         this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities
-                            .ListByResourceGroupWithHttpMessagesAsync(
-                                this.ResourceGroupName).GetAwaiter().GetResult();
+                            .ListByResourceGroupWithHttpMessagesAsync(this.ResourceGroupName).GetAwaiter().GetResult();
+                    var resultList = result.Body.ToList();
+                    var nextPageLink = result.Body.NextPageLink;
+                    while (!string.IsNullOrEmpty(nextPageLink))
+                    {
+                        var pageResult = this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities
+                            .ListByResourceGroupNextWithHttpMessagesAsync(nextPageLink).GetAwaiter().GetResult();
+                        resultList.AddRange(pageResult.Body.ToList());
+                        nextPageLink = pageResult.Body.NextPageLink;
+                    }
 
-                    WriteObject(result.Body);
+                    WriteObject(resultList, true);
                 }
                 else
                 {
-                    var result = this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities.GetWithHttpMessagesAsync(
-                        this.ResourceGroupName,
-                        this.Name).GetAwaiter().GetResult();
+                    var result =
+                        this.MsiClient.ManagedServiceIdentityClient.UserAssignedIdentities.GetWithHttpMessagesAsync(
+                            this.ResourceGroupName,
+                            this.Name).GetAwaiter().GetResult();
 
                     WriteObject(result.Body);
                 }
