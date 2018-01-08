a75c328cbc4a34b22d68bf97ffcf07a5d786b460
Varun Kumar <vakuma@microsoft.com>
2018-01-08 15:13:29 -0800

    Adding some tests

---
+++ src/ResourceManager/ManagedServiceIdentity/Commands.ManagedServiceIdentity.Tests/ScenarioTests/UserAssignedIdentityCrudTests.ps1
@@ -0,0 +1,94 @@
+﻿<#
+.SYNOPSIS
+Tests Managed Service Identity UserAssignedIdentities CRUD.
+#>
+function Test-CrudUserAssignedIdentity
+{
+    # Setup
+    $location = Get-ProviderLocation "Microsoft.ManagedServiceIdentity/service"
+    $resourceGroupName = Get-ResourceGroupName
+    $apiManagementName = Get-ApiManagementServiceName
+    $organization = "apimpowershellorg"
+    $adminEmail = "apim@powershell.org"
+    $secondApiManagementName = Get-ApiManagementServiceName
+    $secondOrganization = "second.apimpowershellorg"
+    $secondAdminEmail = "second.apim@powershell.org"
+    $secondSku = "Standard"
+    $secondSkuCapacity = 2
+
+    try
+    {
+        # Create Resource Group
+        New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+        
+        # Create API Management service
+        $result = New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Location $location -Name $apiManagementName -Organization $organization -AdminEmail $adminEmail
+
+        Assert-AreEqual $resourceGroupName $result.ResourceGroupName
+        Assert-AreEqual $apiManagementName $result.Name
+        Assert-AreEqual $location $result.Location
+        Assert-AreEqual "Developer" $result.Sku
+        Assert-AreEqual 1 $result.Capacity
+        Assert-AreEqual "None" $result.VpnType
+
+        # Get SSO token
+        $token = Get-AzureRmApiManagementSsoToken -ResourceGroupName $resourceGroupName -Name $apiManagementName
+        Assert-NotNull $token
+
+        # List services within the resource group
+        $apimServicesInGroup = Get-AzureRmApiManagement -ResourceGroupName $resourceGroupName
+        Assert-True {$apimServicesInGroup.Count -ge 1}
+        
+        #Create Second Service
+        $secondResult = New-AzureRmApiManagement -ResourceGroupName $resourceGroupName -Location $location -Name $secondApiManagementName -Organization $secondOrganization -AdminEmail $secondAdminEmail -Sku $secondSku -Capacity $secondSkuCapacity
+        Assert-AreEqual $resourceGroupName $secondResult.ResourceGroupName
+        Assert-AreEqual $secondApiManagementName $secondResult.Name
+        Assert-AreEqual $location $secondResult.Location
+        Assert-AreEqual $secondSku $secondResult.Sku
+        Assert-AreEqual $secondSkuCapacity $secondResult.Capacity
+
+        # Get SSO token
+        $secondToken = Get-AzureRmApiManagementSsoToken -ResourceGroupName $resourceGroupName -Name $secondApiManagementName
+        Assert-NotNull $secondToken
+
+        # List all services
+        $allServices = Get-AzureRmApiManagement
+        Assert-True {$allServices.Count -ge 2}
+        
+        $found = 0
+        for ($i = 0; $i -lt $allServices.Count; $i++)
+        {
+            if ($allServices[$i].Name -eq $apiManagementName)
+            {
+                $found = $found + 1
+                Assert-AreEqual $location $allServices[$i].Location
+                Assert-AreEqual $resourceGroupName $allServices[$i].ResourceGroupName
+        
+                Assert-AreEqual "Developer" $allServices[$i].Sku
+                Assert-AreEqual 1 $allServices[$i].Capacity
+            }
+
+            if ($allServices[$i].Name -eq $secondApiManagementName)
+            {
+                $found = $found + 1
+                Assert-AreEqual $location $allServices[$i].Location
+                Assert-AreEqual $resourceGroupName $allServices[$i].ResourceGroupName
+        
+                Assert-AreEqual $secondSku $allServices[$i].Sku
+                Assert-AreEqual $secondSkuCapacity $allServices[$i].Capacity
+            }
+        }
+        Assert-True {$found -eq 2} "Api Management services created earlier is not found."
+        
+         # Delete listed services in the ResourceGroup
+        Get-AzureRmApiManagement -ResourceGroupName $resourceGroupName | Remove-AzureRmApiManagement
+
+        $allServices = Get-AzureRmApiManagement -ResourceGroupName $resourceGroupName
+        Assert-AreEqual 0 $allServices.Count
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $resourceGroupName
+    }
+}
