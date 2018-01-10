f024316be8bc4611f20dc4aac21b5ba9d0532303
Varun Kumar <vakuma@microsoft.com>
2018-01-10 13:34:19 -0800

    temp

--- src/ResourceManager/ManagedServiceIdentity/Commands.ManagedServiceIdentity.Tests/ScenarioTests/UserAssignedIdentityCrudTests.ps1
+++ src/ResourceManager/ManagedServiceIdentity/Commands.ManagedServiceIdentity.Tests/ScenarioTests/UserAssignedIdentityCrudTests.ps1
@@ -4,66 +4,87 @@ Tests Managed Service Identity UserAssignedIdentities CRUD.
 #>
 function Test-CrudUserAssignedIdentity
 {
-    $rgName = "MSIPSTestRG";
+    $rgName1 = "MSIPSTestRG1";
+    $rgName2 = "MSIPSTestRG2";
     $identityType = "Microsoft.ManagedIdentity/userAssignedIdentities"
     $firstTagKey = "tag1";
     $firstTagValue = "firstTag";
     $secondTagKey = "tag2";
     $secondTagValue = "secondTag";
+    $subscriptionId = "fa5fc227-a624-475e-b696-cdd604c735bc";
 
     try
     {
-        $identityName = "MSIPSTestIdentity";
+        $identityName1 = "MSIPSTestIdentity1";
+        $identityName2 = "MSIPSTestIdentity2";
         $loc = "westus";
-        $expectedIdentityID = "/subscriptions/fa5fc227-a624-475e-b696-cdd604c735bc/resourcegroups/"+$rgName+"/providers/Microsoft.ManagedIdentity/userAssignedIdentities/"+$identityName
-        #$expectedIdentityID | Out-File "C:\Users\vakuma\Desktop\resultID2.txt";
-        #Create new Resource Group
-        $rg = New-AzureRmResourceGroup -Name $rgName -Location $loc;
 
-        #Create new Identity
-        $identity = New-AzureRmUserAssignedIdentity -ResourceGroupName $rgName -Name $identityName -Location $loc;
-        Assert-NotNull $identity;
-        Assert-AreEqual $identity.ID $expectedIdentityID;
-        Assert-AreEqual $identity.Name $identityName;
-        Assert-AreEqual $identity.Location $loc;
-        Assert-AreEqual $identity.Type $identityType;
-        #$identity | Out-File "C:\Users\vakuma\Desktop\resultID.txt";
+        #Create Resource Group 1
+        $rg1 = New-AzureRmResourceGroup -Name $rgName1 -Location $loc;
+        #Create Resource Group 2
+        $rg2 = New-AzureRmResourceGroup -Name $rgName2 -Location $loc;
 
-        #Update Identity
-        $identity = Set-AzureRmUserAssignedIdentity -ResourceGroupName $rgName -Name $identityName -Tag @{$firstTagKey=$firstTagValue; $secondTagKey=$secondTagValue};
-        $identity | Out-File "C:\Users\vakuma\Desktop\resultID.txt";
-        Assert-NotNull $identity;
-        Assert-AreEqual $identity.ID $expectedIdentityID;
-        Assert-AreEqual $identity.Name $identityName;
-        Assert-AreEqual $identity.Location $loc;
-        Assert-AreEqual $identity.Type $identityType;
-        Assert-AreEqual $identity.Tags.Count 2;
-        Assert-AreEqual $identity.Tags.ContainsKey($firstTagKey) True;
-        Assert-AreEqual $identity.Tags[$firstTagKey] $firstTagValue;
-        Assert-AreEqual $identity.Tags.ContainsKey($secondTagKey) True;
-        Assert-AreEqual $identity.Tags[$secondTagKey] $secondTagValue;
+        #Create Identity1 under Resource Group 1
+        $identity1 = New-AzureRmUserAssignedIdentity -ResourceGroupName $rgName1 -Name $identityName1 -Location $loc;
+        $expectedIdentityId = Get-ExpectedIdentityId -subscription $subscriptionId -rg $rgName1 -identity $identityName1;
+        $expectedIdentityId | Out-File "C:\Users\vakuma\Desktop\resultID2.txt";
+        $identity1.ID | Out-File "C:\Users\vakuma\Desktop\resultID.txt";
+        Assert-NotNull $identity1;
+        Assert-AreEqual $identity1.ID $expectedIdentityId;
+        Assert-AreEqual $identity1.Name $identityName1;
+        Assert-AreEqual $identity1.Location $loc;
+        Assert-AreEqual $identity1.Type $identityType;
 
-        #Get Identity
-        $identity = Get-AzureRmUserAssignedIdentity -ResourceGroupName $rgName -Name $identityName
-        Assert-NotNull $identity;
-        Assert-AreEqual $identity.ID $expectedIdentityID;
-        Assert-AreEqual $identity.Name $identityName;
-        Assert-AreEqual $identity.Location $loc;
-        Assert-AreEqual $identity.Type $identityType;
-        Assert-AreEqual $identity.Tags.Count 2;
-        Assert-AreEqual $identity.Tags.ContainsKey($firstTagKey) True;
-        Assert-AreEqual $identity.Tags[$firstTagKey] $firstTagValue;
-        Assert-AreEqual $identity.Tags.ContainsKey($secondTagKey) True;
-        Assert-AreEqual $identity.Tags[$secondTagKey] $secondTagValue;
+        #Create Identity2 under Resource Group 2
+        $identity2 = New-AzureRmUserAssignedIdentity -ResourceGroupName $rgName2 -Name $identityName2 -Location $loc;
+        $expectedIdentityId = Get-ExpectedIdentityId -subscription $subscriptionId -rg $rgName2 -identity $identityName2;
+        Assert-NotNull $identity2;
+        Assert-AreEqual $identity2.ID $expectedIdentityId;
+        Assert-AreEqual $identity2.Name $identityName2;
+        Assert-AreEqual $identity2.Location $loc;
+        Assert-AreEqual $identity2.Type $identityType;
+
+        #Update Identity 1
+        $identity1 = Set-AzureRmUserAssignedIdentity -ResourceGroupName $rgName1 -Name $identityName1 -Tag @{$firstTagKey=$firstTagValue; $secondTagKey=$secondTagValue};
+        Assert-NotNull $identity1;
+        Assert-AreEqual $identity1.ID $expectedIdentityID1;
+        Assert-AreEqual $identity1.Name $identityName1;
+        Assert-AreEqual $identity1.Location $loc;
+        Assert-AreEqual $identity1.Type $identityType;
+        Assert-AreEqual $identity1.Tags.Count 2;
+        Assert-AreEqual $identity1.Tags.ContainsKey($firstTagKey) True;
+        Assert-AreEqual $identity1.Tags[$firstTagKey] $firstTagValue;
+        Assert-AreEqual $identity1.Tags.ContainsKey($secondTagKey) True;
+        Assert-AreEqual $identity1.Tags[$secondTagKey] $secondTagValue;
+
+        #Get Identity 1
+        $identity1 = Get-AzureRmUserAssignedIdentity -ResourceGroupName $rgName1 -Name $identityName1
+        Assert-NotNull $identity1;
+        Assert-AreEqual $identity1.ID $expectedIdentityID1;
+        Assert-AreEqual $identity1.Name $identityName1;
+        Assert-AreEqual $identity1.Location $loc;
+        Assert-AreEqual $identity1.Type $identityType;
+        Assert-AreEqual $identity1.Tags.Count 2;
+        Assert-AreEqual $identity1.Tags.ContainsKey($firstTagKey) True;
+        Assert-AreEqual $identity1.Tags[$firstTagKey] $firstTagValue;
+        Assert-AreEqual $identity1.Tags.ContainsKey($secondTagKey) True;
+        Assert-AreEqual $identity1.Tags[$secondTagKey] $secondTagValue;
+
+        #Get identities under Subscription
+        $identities = Get-AzureRmUserAssignedIdentity
+        Assert-AreEqual $identities.Count 2
 
         #Delete Identity
-        Remove-AzureRmUserAssignedIdentity -ResourceGroupName $rgName -Name $identityName;
-        $resourceGroupIdentities = Get-AzureRmUserAssignedIdentity -ResourceGroupName $rgName
+        Remove-AzureRmUserAssignedIdentity -ResourceGroupName $rgName1 -Name $identityName1;
+        $resourceGroupIdentities = Get-AzureRmUserAssignedIdentity -ResourceGroupName $rgName1
         Assert-Null $resourceGroupIdentities;
+
+        
     }
     finally
     {
-        Clean-ResourceGroup $rgname;
+        Clean-ResourceGroup $rgname1;
+        Clean-ResourceGroup $rgname2;
     }
 }
 
@@ -77,4 +98,9 @@ function Clean-ResourceGroup($rgname)
     {
         Remove-AzureRmResourceGroup -Name $rgname -Force
     }
+}
+
+function Get-ExpectedIdentityId($subscription, $rg, $identity)
+{
+    return "/subscriptions/"+$subscription+"/resourcegroups/"+$rg+"/providers/Microsoft.ManagedIdentity/userAssignedIdentities/"+$identity;
 }
